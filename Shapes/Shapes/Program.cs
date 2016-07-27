3b50b9c6b985824b39512d13cd28a9a48e5188f9
ryan stevens <ryan stevens>
2016-07-27 14:37:18 -0700

    First step Notice how the circle is not overriden and so it outputs what is in the Shapes class.

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Polymorphism *****");
+
+            Hexagon hex = new Hexagon("Beth");
+            hex.Draw();
+            Circle cir = new Circle("Cindy");
+            //Calls base class implementation
+            cir.Draw();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Shapes/Shapes/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    // the abstract class hierarachy
+    abstract class Shape
+    {
+        public Shape(String name = "NoName")
+        { petName = name; }
+
+        public string petName { get; set; }
+
+        // A single virtual method
+        public virtual void Draw()
+        {
+            Console.WriteLine("Inside Shape.Draw()");
+        }
+    }
+}
