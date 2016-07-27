ba6b891e1d03c68322f9236a7c442d63e26bc186
ryan stevens <ryan stevens>
2016-07-27 15:38:24 -0700

    created a new class to give an example of shadowing

---
+++ Shapes/Shapes/ThreeDCircle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class ThreeDCircle : Circle
+    {
+        // hide the petName property above me
+        public new string PetName { get; set; }
+
+        // here is an example of Shadowing
+        public new void Draw()
+        {
+            Console.WriteLine("Drawing a 3D circle");
+        }
+    }
+}
